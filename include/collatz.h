780499b397aee602e0f1320599b005279530a898
Quentin <QDM@localhost.localdomain>
2019-11-27 15:07:36 +0100

    [ADD] include: adding the project's library and headers

---
+++ include/collatz.h
@@ -0,0 +1,15 @@
+/*
+** PERSONAL PROJECT, 2019
+** collatz
+** File description:
+** collatz computer headers
+*/
+
+#ifndef COLLATZ_H_
+#define COLLATZ_H_
+
+int err_invalid_args(void);
+int err_nb_args(void);
+int help(void);
+
+#endif
